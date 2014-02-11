811aa9f01cfe71125627615b389ca06efb4ff4bf
kklai <kkrebeccalai@gmail.com>
2014-02-11 10:37:26 -0600

    mostly working

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-var input, cal, num, value, operator;
+var input, cal, num, value, operator, pm;
 
 function readOperand(n){
 	num = n[0];
 	n.shift();
 	num = parseInt(num)
-	if (isNaN(num) === true) {
+	if (n[0] === "-") {
+		num = n[0] + n[1];
+		num = parseInt(num)
+		n.shift();
+		n.shift();
+		return num
+	} else if (isNaN(num) == true) {
 		return "First input must be a number."
-	} else if (n[0] === "-") {
-		console.log('negative operand')
 	} else {
 		return num
 	}
@@ -28,9 +32,10 @@ function evaluate(n) {
 			value = value - temp;
 		} else if (operator === "x") {
 			value = value * temp;
-		} else if (operator === "÷") {
+		} else if (operator === "/") {
 			value = value/temp;
-		} else {
+		} else { 
+			console.log(operator);
 			console.log('Unrecognized operator');
 		}
 	}
@@ -59,6 +64,10 @@ $(document).ready(function(){
 		if (input != '=') {
 			$(".display-text").append(input);
 		}
+	})
+
+	$('#plusminus').click(function(){
+		pm = $('.display-text').innerHTML;
 	})
 
 	$('#clear').click(function(){
