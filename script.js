a72e1f2873a479890e8e481680c69593f64d4a0b
kklai <kkrebeccalai@gmail.com>
2014-02-17 02:18:29 -0600

    kind of working...

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var input, cal, num, value, operator, pm, check;
+var input, cal, num, value, operator, pm, check, new_temp, new_op, new_value;
 
 function readOperand(n){
 	num = n[0];
@@ -7,7 +7,6 @@ function readOperand(n){
 		num = n[0] + n[1]
 		n.shift();
 		n.shift();
-		console.log(n)
 	}
 	num = parseInt(num, 10)
 	if (isNaN(num) == true) {
@@ -23,28 +22,40 @@ function evaluate(n) {
 	}
 	value = readOperand(n)
 	while (n.length != 0) {
+		console.log(n);
 		operator = n[0];
 		n.shift();
 		temp = readOperand(n)
-		if (operator === "+") {
-			value = value + temp
-		} else if (operator === "-") {
-			value = value - temp;
-		} else if (operator === "x") {
+		if (operator === "x") {
 			value = value * temp;
 		} else if (operator === "/"){
 			value = value / temp
-		} else {
-			console.log('Unrecognized operator');
+		} else if ((n.length > 1) && ((operator === "+") || (operator === "-")) && ((n.indexOf("x") != -1) || (n.indexOf("/") != -1))) {
+			saved_op = operator;
+			new_op = n.shift();
+			new_temp = parseInt(n.shift());
+			if (new_op === "x") {
+				new_value = temp * new_temp;
+			} else if (new_op === "/") {
+				new_value = temp / new_temp
+			}
+			if (operator === "+") {
+				value = value + new_value
+			} else if (operator === "-") {
+				value = value - new_value
+			}
+		} else if (operator === "+") {
+			value = value + temp
+		} else if (operator === "-") {
+			value = value - temp
 		}
 	}
-	return value
+	return value;
 }
 
 function calculate() {
 	cal = $('.display-text')[0].innerHTML;
 	cal = cal.split('&nbsp;')
-	console.log(cal);
 	try {
 		val = evaluate(cal)
 		$('.display-text').html(value);
@@ -67,7 +78,6 @@ $(document).ready(function(){
 
 	$('#plusminus').click(function(){
 		calculate();
-		// console.log(val);
 		if (val < 0) {
 			val = Math.abs(val)
 		} else {
